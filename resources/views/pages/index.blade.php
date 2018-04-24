edbc204e6fc1689dabc180dbfcd6719abf9fe26f
Dinushan <dinushanms@gmail.com>
2018-04-24 09:18:18 +0530

    CRUD

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Welcom to Laravel!</h1>
+        <p>This is a laravel app..</p>
+        <p><a class="btn btn-primary btn-lg" href="login" role="button">Login</a></p>
+        <p><a class="btn btn-success btn-lg" href="register" role="button">Register</a></p>
+    </div>
+@endsection
